3859b708286586ce7a6e0af2a23d028920673675
BLADE-HEDA <you@example.com>
2024-01-05 13:38:46 +0100

    refactored code

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -85,8 +85,5 @@ function LoginButton() {
     <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
-      //   <Button className="mt-4 w-full" aria-disabled={pending}>
-      //   Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-      // </Button>
   );
 }
