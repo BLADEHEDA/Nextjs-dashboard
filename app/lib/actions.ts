233b1fce7dfcfae66e7ee71fe1ae759613ec9dcb
BLADE-HEDA <you@example.com>
2024-01-05 13:37:08 +0100

    learned about authentication in nextjs , but needsto be relearned

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,10 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { z } from 'zod';
 import { redirect } from 'next/navigation';
- 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth'; 
+
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -117,5 +120,25 @@ export async function deleteInvoice(id: string) {
       return { message: 'Deleted Invoice' };
     } catch (error) {
       return { message: 'Database Error: Failed to Delete Invoice' };
+    }
+  }
+
+  // function to authenticate 
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
     }
   }
